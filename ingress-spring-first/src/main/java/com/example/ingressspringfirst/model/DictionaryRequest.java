05675e17988cd05891aeab31211786ab1ec41562
Ismat Aghamirzayev <ismataga1@gmail.com>
2023-09-01 15:05:26 +0400

    add liquebase

--- ingress-spring-first/src/main/java/com/example/ingressspringfirst/model/DictionaryRequest.java
+++ ingress-spring-first/src/main/java/com/example/ingressspringfirst/model/DictionaryRequest.java
@@ -1,13 +1,20 @@
 package com.example.ingressspringfirst.model;
 
+import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class DictionaryRequest {
-    private String category;
-    private String subCategory;
+    private String username;
+    private Integer age;
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
 }
