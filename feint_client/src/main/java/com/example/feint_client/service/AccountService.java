cdded80caf1c232e445b29e7fdc7195406688eba
Ismat Aghamirzayev <ismataga1@gmail.com>
2023-09-14 15:01:13 +0400

    chane  accountService

--- feint_client/src/main/java/com/example/feint_client/service/AccountService.java
+++ feint_client/src/main/java/com/example/feint_client/service/AccountService.java
@@ -1,9 +1,7 @@
 package com.example.feint_client.service;
 
-import static com.example.feint_client.mapper.UserMapper.buildUserRequest;
 import static com.example.feint_client.model.constant.CurrencyConstant.eligibleCurrencies;
 
-import com.example.feint_client.client.UserClient;
 import com.example.feint_client.model.client.AccountRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -11,13 +9,12 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class AccountService {
-    private final UserClient userClient;
 
+    private final AsyncService asyncService;
     public void validateAccount(AccountRequest account) {
-        if (eligibleCurrencies.contains(account.getCurrency())) {
-            throw new RuntimeException("NOT ELIGIBLE CIRRENCY");
+        if (!eligibleCurrencies.contains(account.getCurrency())) {
+            throw new RuntimeException("NOT ELIGIBLE CURRENCY");
         }
-
-        userClient.addUser(buildUserRequest());
+        asyncService.saveUser();
     }
 }
